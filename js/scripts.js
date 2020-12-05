b54ba0855e69d47a25f5eda610a7001801e4ef07
Bilal Khan <mynamebilalkhan@gmail.com>
2020-12-06 01:12:08 +0500

    second revision

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,15 @@ jQuery(document).ready(function($){
         $('.navigation-section').toggleClass('active');
         $('.destination-dropdown').toggleClass('active');
     });
+    jQuery("#location_triger").on('click', function(){
+        $('.location-dropdown-container').toggleClass('active');
+        $('#location_triger').toggleClass('active');
+    });
+    jQuery("#date_picker").on('click', function(){
+        $('.checkin-checkout-calendar').toggleClass('active');
+    });
+    jQuery("#guests_trigger").on('click', function(){
+        $('.guests-dropdown-container').toggleClass('active');
+        $('.guests_trigger').toggleClass('active');
+    });
 });
